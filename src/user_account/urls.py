8ebf5f4d82fdbdb28b913f44fc8c01ac8802e8df
Ivan <ivan@Ivans-MacBook-Pro-3.local>
2020-05-30 12:22:37 +0300

    Hometask #13

--- src/user_account/urls.py
+++ src/user_account/urls.py
@@ -4,11 +4,12 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 from student.views import generate_student
-from user_account.views import CreateUserAccountView
+from user_account.views import CreateUserAccountView, SuccessRegistrationView
 
 app_name = 'user_account'
 
 urlpatterns = [
     path('register/', CreateUserAccountView.as_view(), name='registration'),
+    path('success-registration/', SuccessRegistrationView.as_view(), name='success-registration'),
 
 ]
--- src/user_account/views.py
+++ src/user_account/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.views.generic.edit import BaseFormView
 
 from user_account.forms import UserAccountRegistrationForm
 
@@ -11,9 +12,18 @@ class CreateUserAccountView(CreateView):
     form_class = UserAccountRegistrationForm
 
     def get_success_url(self):
-        return reverse('index')
+        return reverse('success-registration')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(object_list=None, **kwargs)
         context['title'] = 'Register new user'
+        return context
+
+
+class SuccessRegistrationView(TemplateView):
+    template_name = 'success.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['title'] = 'Successfully created a user'
         return context
